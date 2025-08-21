84a4d4ce1f41cc33195c28b4c18162f87aa4ff18
EvgeniyShell <37295805+EvgeniyShell@users.noreply.github.com>
2025-08-21 17:13:38 +1100

    Update update-checker.ts

--- packages/api/src/router/update-checker.ts
+++ packages/api/src/router/update-checker.ts
@@ -5,13 +5,14 @@ import { createTRPCRouter, permissionRequiredProcedure } from "../trpc";
 
 export const updateCheckerRouter = createTRPCRouter({
   getAvailableUpdates: permissionRequiredProcedure.requiresPermission("admin").query(async () => {
-    try {
-      const handler = updateCheckerRequestHandler.handler({});
-      const data = await handler.getCachedOrUpdatedDataAsync({});
-      return data.data.availableUpdates;
-    } catch (error) {
-      logger.error(new Error("Failed to get available updates", { cause: error }));
-      return undefined; // We return undefined to not show the indicator in the UI
-    }
+    return undefined
+    // try {
+    //   const handler = updateCheckerRequestHandler.handler({});
+    //   const data = await handler.getCachedOrUpdatedDataAsync({});
+    //   return data.data.availableUpdates;
+    // } catch (error) {
+    //   logger.error(new Error("Failed to get available updates", { cause: error }));
+    //   return undefined; // We return undefined to not show the indicator in the UI
+    // }
   }),
 });
